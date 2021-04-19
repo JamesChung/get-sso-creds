c10daa9533016e5317b7c43ce92f6412279a34d1
James Chung <james.chung@linux.com>
2021-04-19 18:50:55 -0500

    Add error handing

--- src/lib/output-helper.ts
+++ src/lib/output-helper.ts
@@ -6,13 +6,18 @@ export async function output(command: Command, profile: string) {
   if (!isProfile(profile)) {
     throw `> [ ${profile} ] is not a valid profile.`;
   }
-  const profileInfo = getProfileInfo(profile);
-  profileInfo.identity = await initCredentials(profileInfo.profileName);
-  const credentials = await getCredentials(profileInfo);
 
-  command.log(`Profile: ${profileInfo.profileName}`);
-  command.log(`Credentials expire at: ${(new Date(credentials.expiration)).toLocaleTimeString()}\n`);
-  command.log(`export AWS_ACCESS_KEY_ID=${credentials.accessKeyId}`);
-  command.log(`export AWS_SECRET_ACCESS_KEY=${credentials.secretAccessKey}`);
-  command.log(`export AWS_SESSION_TOKEN=${credentials.sessionToken}`);
+  try {
+    const profileInfo = getProfileInfo(profile);
+    profileInfo.identity = await initCredentials(profileInfo.profileName);
+    const credentials = await getCredentials(profileInfo);
+  
+    command.log(`Profile: ${profileInfo.profileName}`);
+    command.log(`Credentials expire at: ${(new Date(credentials.expiration)).toLocaleTimeString()}\n`);
+    command.log(`export AWS_ACCESS_KEY_ID=${credentials.accessKeyId}`);
+    command.log(`export AWS_SECRET_ACCESS_KEY=${credentials.secretAccessKey}`);
+    command.log(`export AWS_SESSION_TOKEN=${credentials.sessionToken}`);
+  } catch (error) {
+    console.log(error);
+  }
 }
