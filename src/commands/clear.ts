050f989ddae7ef567b616f7bee48a8c13bec217e
James Chung <james.chung@linux.com>
2021-04-20 12:17:49 -0500

    Add clear command

---
+++ src/commands/clear.ts
@@ -0,0 +1,29 @@
+import { Command, flags } from '@oclif/command';
+import { clearCredentials } from '../lib/creds-helper';
+import cli from 'cli-ux';
+
+export default class Clear extends Command {
+  static description = 'clears credentials in ~/.aws/credentials';
+
+  static examples = [
+    `$ gsc clear`,
+  ];
+
+  static flags = {
+    help: flags.help({ char: 'h', description: undefined }),
+  };
+
+  static args = [];
+
+  async run() {
+    const {args, flags} = this.parse(Clear);
+
+    try {
+      cli.action.start('Clearing');
+      clearCredentials(this);
+      cli.action.stop();
+    } catch (error) {
+      this.log(error);
+    }
+  }
+}
