7351de37b6b0ed44fc4c32ae59d269f4cc120669
James Chung <james@chung.contact>
2022-03-27 21:11:37 -0500

    Version 5.2.0

---
+++ src/lib/assume-helper.ts
@@ -0,0 +1,22 @@
+import { exec } from 'child_process';
+import { ICredentials } from '../lib/interfaces';
+
+export async function assumeRole(roleArn: string, sessionName: string, profile: string): Promise<ICredentials> {
+  return new Promise((resolve, reject) => {
+    exec(`aws sts assume-role --role-arn ${roleArn} --role-session-name ${sessionName} --profile ${profile}`, (error, stdout, stderr) => {
+      if (stderr) {
+        reject(new Error(stderr));
+      }
+      if (stdout) {
+        const parsedValue = JSON.parse(stdout);
+        const returnValue: ICredentials = {
+          accessKeyId: parsedValue.Credentials.AccessKeyId,
+          secretAccessKey: parsedValue.Credentials.SecretAccessKey,
+          sessionToken: parsedValue.Credentials.SessionToken,
+          expiration: parsedValue.Credentials.Expiration,
+        };
+        resolve(returnValue);
+      }
+    });
+  });
+}
