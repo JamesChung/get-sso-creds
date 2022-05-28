d7ddc3b786256c21d485b3dde1194422878f6b94
James Chung <james@chung.contact>
2022-05-27 19:01:48 -0500

    update

--- src/commands/console.ts
+++ src/commands/console.ts
@@ -1,9 +1,10 @@
 import { Command, Flags, CliUx } from '@oclif/core';
-import { getProfileCredentials } from '../lib/creds-helper';
+import { getProfileCredentials, getCredentialsFromCredentialsFile } from '../lib/creds-helper';
 import { generateLoginURL } from '../lib/console-helper';
-import { getCredProfiles } from '../lib/profile-helper';
+import { getCredProfiles, getProfileNames } from '../lib/profile-helper';
 import * as inquirer from 'inquirer';
 import * as open from 'open';
+import { ICredentials } from '../lib';
 
 export default class Console extends Command {
   static description = 'Opens AWS Console for a selected profile.';
@@ -17,27 +18,42 @@ export default class Console extends Command {
 
   static flags = {
     help: Flags.help(),
-    profile: Flags.string({
-      char: 'p',
-      default: 'default',
-      description: 'Desired SSO config profile to use.'
+    browser: Flags.string({
+
     }),
   };
 
   static args = [];
 
   public async run(): Promise<void> {
+    const { flags } = await this.parse(Console);
+
     try {
-      const response = await inquirer.prompt([{
+      const { profileType } = await inquirer.prompt([{
+        name: 'profileType',
+        message: 'Select a file:',
+        type: 'list',
+        choices: ['config', 'credentials'],
+      }]);
+
+      const { profile } = await inquirer.prompt([{
         name: 'profile',
         message: 'Select a profile:',
         type: 'list',
-        choices: getCredProfiles()
+        choices: profileType === 'config' ? getProfileNames() : getCredProfiles(),
       }]);
+
       CliUx.ux.action.start('❯ Opening Console');
-      const info = await getProfileCredentials(response?.profile);
-      const loginURL = await generateLoginURL(info.credentials);
-      open(loginURL, {});
+      if (profileType === 'config') {
+        const credentials = (await getProfileCredentials(profile)).credentials;
+        const loginURL = await generateLoginURL(credentials);
+        open(loginURL, {});
+      }
+      if (profileType === 'credentials') {
+        const credentials = getCredentialsFromCredentialsFile(profile);
+        const loginURL = await generateLoginURL(credentials);
+        open(loginURL, {});
+      }
       CliUx.ux.action.stop();
     } catch (error: any) {
       CliUx.ux.action.stop('failed');
